44a7e6b2b56dfc74a75d1963f2ff1369013cda9d
Dmitriy Ugarov <d.ugarov@genesis.vision>
2018-02-02 11:01:43 +0300

    Project init

---
+++ GenesisVision.DataModel/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace GenesisVision.DataModel
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options)
+            : base(options)
+        {
+        }
+    }
+}
---
+++ GenesisVision.Tournament.Core/Startup.cs
@@ -0,0 +1,78 @@
+﻿using GenesisVision.DataModel;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Swashbuckle.AspNetCore.Swagger;
+using System;
+using System.IO;
+using System.Linq;
+
+namespace GenesisVision.Tournament.Core
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            var connectionString = Configuration["DbContextSettings:ConnectionString"];
+            var dbContextOptions = new Action<NpgsqlDbContextOptionsBuilder>(options =>
+                options.MigrationsAssembly("GenesisVision.Tournament.Core"));
+
+            services.AddEntityFrameworkNpgsql()
+                    .AddDbContext<ApplicationDbContext>(x => x.UseNpgsql(connectionString, dbContextOptions));
+
+            services.AddMvcCore()
+                    .AddApiExplorer()
+                    .AddDataAnnotations()
+                    .AddJsonFormatters()
+                    .AddJsonOptions(options =>
+                    {
+                        options.SerializerSettings.DateFormatString = "yyyy-MM-ddTHH\\:mm\\:ss.fffffffzzz";
+                    });
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info
+                                   {
+                                       Title = "Tournament Core API",
+                                       Version = "v1",
+                                       Contact = new Contact
+                                                 {
+                                                     Name = "Genesis Vision",
+                                                     Url = "https://genesis.vision/"
+                                                 }
+                                   });
+                c.DescribeAllEnumsAsStrings();
+                c.TagActionsBy(x => x.RelativePath.Split("/").Take(2).Last());
+
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, "GenesisVision.Tournament.Core.xml");
+                c.IncludeXmlComments(xmlPath);
+            });
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseMvcWithDefaultRoute();
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Tournament Core API v1");
+            });
+        }
+    }
+}
