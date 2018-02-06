3422952f512534cea72ecd705579b07ed7e2b211
Dmitriy Ugarov <d.ugarov@genesis.vision>
2018-02-06 13:27:03 +0300

    Local requests
    (cherry picked from commit afae42cfcc68d9c84846491e6f4041c6ed0c9321)

---
+++ GenesisVision.Tournament.Core/Infrastructure/HttpContextFilters.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.Net;
+
+namespace GenesisVision.Tournament.Core.Infrastructure
+{
+    public static class HttpContextFilters
+    {
+        public static bool IsLocalRequest(this HttpContext context)
+        {
+            return context.Connection.RemoteIpAddress.Equals(context.Connection.LocalIpAddress) ||
+                   IPAddress.IsLoopback(context.Connection.RemoteIpAddress);
+        }
+    }
+}
